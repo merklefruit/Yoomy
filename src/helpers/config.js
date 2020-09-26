dd130a11741eafad27ce21b707d934b100e79b2c
Nicolas <nicolas@racchi.com>
2020-09-26 11:44:16 +0200

    Update: Fix .env var to use REACT_APP_

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -3,7 +3,7 @@
 // Base API Route
 
 // export const API_URL = "https://yooga-server.herokuapp.com";
-export const API_URL = process.env.API_URL;
+export const API_URL = process.env.REACT_APP_API_URL;
 
 // Dynamic URLs just for simplicity:
 export const USERS_URL = API_URL + "/users";
