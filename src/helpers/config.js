0ed3d988e2b1bb5c1a903e8d7b9b976dc49bb2ac
Nicolas <nicolas@racchi.com>
2020-09-26 11:34:02 +0200

    Added: ENV variable for API url

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,10 +1,9 @@
 //?   GLOBAL CONFIG
 
 // Base API Route
-// Only change this API_URL variable here, other ones are dynamic:
 
 // export const API_URL = "https://yooga-server.herokuapp.com";
-export const API_URL = "http://localhost:5000";
+export const API_URL = process.env.API_URL;
 
 // Dynamic URLs just for simplicity:
 export const USERS_URL = API_URL + "/users";
