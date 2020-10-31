9c4e8066be3db9d3903da35e31792c498cf87ecc
Nicolas <nicolas@racchi.com>
2020-10-31 13:49:28 +0100

    Updated: calendar (next weeks)

--- src/components/_app/_calendarioPage/customCalendar.js
+++ src/components/_app/_calendarioPage/customCalendar.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
 // Date handling
-import { format, addDays } from "date-fns";
+import { format, addDays, subDays } from "date-fns";
 import { it } from "date-fns/locale";
 
 const CustomCalendar = ({ onDateChange }) => {
-  const today = new Date();
+  const [initial, setInitial] = useState(new Date());
   const formatDay = (offset) =>
-    format(addDays(today, offset), "iii dd", { locale: it });
-  const handleClick = (offset) => onDateChange(addDays(today, offset));
+    format(addDays(initial, offset), "iii dd", { locale: it });
+  const handleClick = (offset) => onDateChange(addDays(initial, offset));
 
   const daysToShow = 7;
+  const maxWeeksAhead = 2;
 
   return (
     <Wrapper>
+      <div className="weeks">
+        {initial.getUTCDate() !==
+          addDays(new Date(), daysToShow * maxWeeksAhead).getUTCDate() && (
+          <motion.button
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            onClick={() => setInitial(addDays(initial, daysToShow))}
+          >
+            {"< prossima settimana"}
+          </motion.button>
+        )}
+        {initial.getUTCDate() !== new Date().getUTCDate() && (
+          <motion.button
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            onClick={() => setInitial(subDays(initial, daysToShow))}
+          >
+            {"settimana precedente >"}
+          </motion.button>
+        )}
+      </div>
       <Calendar>
         {Array.from(Array(daysToShow).keys()).map((i) => (
           <div className="daycard" key={i}>
@@ -42,6 +64,45 @@ const Wrapper = styled.div`
   position: relative;
   display: grid;
   place-items: center;
+
+  .weeks {
+    padding-bottom: 20px;
+    display: flex;
+    align-items: flex-end;
+    direction: rtl;
+    justify-content: space-between;
+
+    width: 70%;
+    @media (max-width: 1300px) {
+      width: 75%;
+    }
+    @media (max-width: 1100px) {
+      width: 80%;
+    }
+    @media (max-width: 700px) {
+      width: 100%;
+    }
+
+    button {
+      background: none;
+      border: none;
+      font-family: "DM Sans";
+      font-weight: 300;
+      display: flex;
+      align-items: center;
+
+      cursor: pointer;
+      &:focus {
+        outline: none;
+      }
+
+      svg {
+        padding-left: 5px;
+        width: 20px;
+        padding-top: 2px;
+      }
+    }
+  }
 `;
 
 const Calendar = styled.div`
