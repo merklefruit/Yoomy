bb48e7630be14004a809d9ab41b2c90e18234e80
Nicolas <nicolas@racchi.com>
2020-12-23 13:14:22 +0100

    Added Sentry in App.js + updated package name

--- src/App.js
+++ src/App.js
@@ -16,7 +16,32 @@ import FullPageSpinner from "./components/fullPageSpinner";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// Conditional routes
+// Sentry monitoring
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+// Sentry initialization
+Sentry.init({
+  dsn:
+    "https://fc88e1f9e8c046ca816c4855045eac15@o495499.ingest.sentry.io/5568329",
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+  // (Sentry recommends adjusting this value in production)
+  // 1.0 sends every issue notifcation
+  tracesSampleRate: 1.0,
+});
+
+/*
+  Yoomy Routing:
+  Yoomy uses auth-based routing, because many different user types can be logged in to the same app.
+  In particular, there are:
+  - VISITORS, which are non-logged-in users
+  - USERS, which are regular yoomy customers
+  - TEACHERS, which have access to their own private routes.
+
+  To avoid long load times, each user type has its own routing, which is lazy-loaded here below.
+*/
+
 const AppRoutes = React.lazy(() =>
   import(/* webpackPrefetch: true */ "./routes/appRoutes")
 );
@@ -71,4 +96,4 @@ const mapStateToProps = (state) => ({
   teacher: state.auth.teacher,
 });
 
-export default connect(mapStateToProps)(App);
+export default Sentry.withProfiler(connect(mapStateToProps)(App));
